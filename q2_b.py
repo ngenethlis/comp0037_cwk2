46a85a376b4c697a1271cc8745e14409124d0667
jameshi16 <1856293+jameshi16@users.noreply.github.com>
2025-03-28 17:17:11 +0000

    tiny change

--- q2_b.py
+++ q2_b.py
@@ -8,52 +8,76 @@ Created on 9 Mar 2023
 
 import math
 
-from common.scenarios import test_2x2_scenario
-from common.scenarios import test_3x3_scenario
-from common.scenarios import test_two_row_scenario
+import matplotlib.pyplot as plt
 
+from analysis_utilities import (get_optimal_policy, matrix_difference_absolute,
+                                matrix_if_differ_difference_absolute,
+                                policy_to_comparable)
 from common.airport_map_drawer import AirportMapDrawer
-
-
+from common.scenarios import (test_2x2_scenario, test_3x3_scenario,
+                              test_three_row_scenario, test_two_row_scenario)
+from generalized_policy_iteration.policy_evaluator import PolicyEvaluator
+from generalized_policy_iteration.value_function_drawer import \
+    ValueFunctionDrawer
+from p1.low_level_actions import LowLevelActionType
+from p1.low_level_environment import LowLevelEnvironment
+from p1.low_level_policy_drawer import LowLevelPolicyDrawer
 from td.q_learner import QLearner
 
-from generalized_policy_iteration.value_function_drawer import ValueFunctionDrawer
-
-from p1.low_level_environment import LowLevelEnvironment
-from p1.low_level_actions import LowLevelActionType
-from p1.low_level_policy_drawer import LowLevelPolicyDrawer
-
 if __name__ == '__main__':
-    airport_map, drawer_height = test_2x2_scenario()
+    # airport_map, drawer_height = test_2x2_scenario()
+    # airport_map, drawer_height = test_3x3_scenario()
+    # airport_map, drawer_height = test_two_row_scenario()
+    airport_map, drawer_height = test_three_row_scenario()
 
-    # Show the scenario        
-    airport_map_drawer = AirportMapDrawer(airport_map, drawer_height)
-    airport_map_drawer.update()
-    
+    # Show the scenario
+    # airport_map_drawer = AirportMapDrawer(airport_map, drawer_height)
+    # airport_map_drawer.update()
+
     # Create the environment
     env = LowLevelEnvironment(airport_map)
 
     # Extract the initial policy. This is e-greedy
     pi = env.initial_policy()
-    
+    pi.set_action(14, 1, LowLevelActionType.MOVE_DOWN)
+    pi.set_action(14, 2, LowLevelActionType.MOVE_DOWN)
+
+    ideal_policy_array = policy_to_comparable(pi)
+
     # Select the controller
-    policy_learner = QLearner(env)   
+    policy_learner = QLearner(env)
     policy_learner.set_initial_policy(pi)
+
+    # According to Slide 34 of "Temporal Difference Learning", Q-Learning:
+    # - (Difficult to get emprical proof of) Convergence is guaranteed if all state and action \
+    #   values are visited an infinite number of times
+    # - Is Off-Policy => Directly computes the optimal policy.
+
+    # Hence, the best way to validate the approach is to compare the optimal policy
+    # with the result we get here.
 
     # These values worked okay for me.
     policy_learner.set_alpha(0.1)
     policy_learner.set_experience_replay_buffer_size(64)
     policy_learner.set_number_of_episodes(32)
-    
+
     # The drawers for the state value and the policy
-    value_function_drawer = ValueFunctionDrawer(policy_learner.value_function(), drawer_height)    
+    value_function_drawer = ValueFunctionDrawer(policy_learner.value_function(), drawer_height)
     greedy_optimal_policy_drawer = LowLevelPolicyDrawer(policy_learner.policy(), drawer_height)
-    
+
+    error_to_ideal = []
+
     for i in range(10000):
-        print(i)
         policy_learner.find_policy()
         value_function_drawer.update()
         greedy_optimal_policy_drawer.update()
         pi.set_epsilon(1/math.sqrt(1+0.25*i))
         print(f"epsilon={1/math.sqrt(1+i)};alpha={policy_learner.alpha()}")
-        
+
+        policy_array = policy_to_comparable(policy_learner.policy())
+        difference_score = matrix_difference_absolute(policy_array, ideal_policy_array)
+        error_to_ideal.append(difference_score)
+
+    plt.title('Policy to Error')
+    plt.plot(list(range(len(error_to_ideal))), error_to_ideal)
+    plt.show()
--- q2_g.py
+++ q2_g.py
@@ -8,47 +8,53 @@ Created on 9 Mar 2023
 
 import math
 
-from common.scenarios import test_2x2_scenario
-from common.scenarios import test_3x3_scenario
-from common.scenarios import test_two_row_scenario
-
+import matplotlib.pyplot as plt
+from analysis_utilities import (get_optimal_policy, matrix_difference_absolute,
+                                matrix_if_differ_difference_absolute,
+                                policy_to_comparable)
 from common.airport_map_drawer import AirportMapDrawer
-
-
+from common.scenarios import (test_2x2_scenario, test_3x3_scenario,
+                              test_two_row_scenario, test_three_row_scenario)
+from generalized_policy_iteration.value_function_drawer import \
+    ValueFunctionDrawer
+from p1.low_level_actions import LowLevelActionType
+from p1.low_level_environment import LowLevelEnvironment
+from p1.low_level_policy_drawer import LowLevelPolicyDrawer
 from td.sarsa import SARSA
 
-from generalized_policy_iteration.value_function_drawer import ValueFunctionDrawer
-
-from p1.low_level_environment import LowLevelEnvironment
-from p1.low_level_actions import LowLevelActionType
-from p1.low_level_policy_drawer import LowLevelPolicyDrawer
-
 if __name__ == '__main__':
-    airport_map, drawer_height = test_2x2_scenario()
+    # airport_map, drawer_height = test_2x2_scenario()
+    airport_map, drawer_height = test_three_row_scenario()
 
-    # Show the scenario        
+    # Show the scenario
     airport_map_drawer = AirportMapDrawer(airport_map, drawer_height)
     airport_map_drawer.update()
-    
+
     # Create the environment
     env = LowLevelEnvironment(airport_map)
 
     # Extract the initial policy. This is e-greedy
     pi = env.initial_policy()
-    
+    pi.set_action(14, 1, LowLevelActionType.MOVE_DOWN)
+    pi.set_action(14, 2, LowLevelActionType.MOVE_DOWN)
+
+    ideal_policy_array = policy_to_comparable(pi)
+
     # Select the controller
-    policy_learner = SARSA(env)   
+    policy_learner = SARSA(env)
     policy_learner.set_initial_policy(pi)
 
     # These values worked okay for me.
     policy_learner.set_alpha(0.1)
     policy_learner.set_experience_replay_buffer_size(64)
     policy_learner.set_number_of_episodes(32)
-    
+
     # The drawers for the state value and the policy
-    value_function_drawer = ValueFunctionDrawer(policy_learner.value_function(), drawer_height)    
+    value_function_drawer = ValueFunctionDrawer(policy_learner.value_function(), drawer_height)
     greedy_optimal_policy_drawer = LowLevelPolicyDrawer(policy_learner.policy(), drawer_height)
-    
+
+    error_to_ideal = []
+
     for i in range(10000):
         print(i)
         policy_learner.find_policy()
@@ -56,4 +62,11 @@ if __name__ == '__main__':
         greedy_optimal_policy_drawer.update()
         pi.set_epsilon(1/math.sqrt(1+0.25*i))
         print(f"epsilon={1/math.sqrt(1+i)};alpha={policy_learner.alpha()}")
-        
+
+        policy_array = policy_to_comparable(policy_learner.policy())
+        difference_score = matrix_difference_absolute(policy_array, ideal_policy_array)
+        error_to_ideal.append(difference_score)
+
+    plt.title('Policy to Error')
+    plt.plot(list(range(len(error_to_ideal))), error_to_ideal)
+    plt.show()
